c28229d217048749e1503c7dfb09b8146ef1dafc
Boxzan594 <xddd382@gmail.com>
2025-07-26 05:16:54 +0700

    ServerRobloxCode

--- script.lua
+++ script.lua
@@ -1,11 +1,10 @@
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local RunService = game:GetService('RunService')
-local R = "RemoteEvent"
 local w = {
-    a = 'Hi',
-    c = "No",
-    e = 'haha'
+    'Hi',
+    "No",
+    'haha'
 }
 while task.wait(math.random(1,5)) do
-    ReplicatedStorage:WaitForChild(R):FireAllClients(w[math.random(1,#w)])
+    ReplicatedStorage:WaitForChild('RemoteEvent'):FireAllClients(w[math.random(1,#w)])
 end
